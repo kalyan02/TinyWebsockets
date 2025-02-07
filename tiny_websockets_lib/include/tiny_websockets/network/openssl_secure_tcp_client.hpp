d431df4e808764b8887f3db837489920c95d7ee6
E Sequeira <5458743+geseq@users.noreply.github.com>
2025-02-07 14:21:54 +0000

    Cleanup casting (#4)
    * cleanup casting

--- tiny_websockets_lib/include/tiny_websockets/network/openssl_secure_tcp_client.hpp
+++ tiny_websockets_lib/include/tiny_websockets/network/openssl_secure_tcp_client.hpp
@@ -25,7 +25,8 @@ namespace websockets { namespace network {
           //fprintf(stdout, "SSL_new() failed\n");
           return false;
         }
-        SSL_set_tlsext_host_name(ssl, host.c_str());
+        SSL_set1_host(ssl, host.c_str());
+        SSL_ctrl(ssl, SSL_CTRL_SET_TLSEXT_HOSTNAME, TLSEXT_NAMETYPE_host_name, const_cast<char*>(host.c_str()));
         bool didConnect = TcpClientImpl::connect(host, port);
         if(didConnect == false) return false;
 
@@ -70,7 +71,7 @@ namespace websockets { namespace network {
         }
         return static_cast<uint32_t>(numReceived);
       }
-      
+
       void close() override {
         if(ssl != nullptr) {
           SSL_free(ssl);
